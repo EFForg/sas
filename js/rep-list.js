c1039ddb629580121195f9986ea53f70799a2d90
matto <matt@mirabot.coop>
2014-06-19 15:30:41 -0700

    Defaulted sort to show A's first per rainey's advice.

--- js/rep-list.js
+++ js/rep-list.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // Add tablesorter and properties.
   $('#reps-all-body').html(sasDisplayAllReps());
   $('#reps-all-table').tablesorter({
-    sortList: [[1,1]],
+    sortList: [[1,0]],
     headers: {
       4: {
          sorter: false,
