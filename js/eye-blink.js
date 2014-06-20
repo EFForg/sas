b6f42d968f210ccc88aadb7914f13c2db6ed044f
squiggy rubio <squiggy@mirabot.coop>
2014-06-20 13:27:50 -0700

    Edit timing of eye blinking.

--- js/eye-blink.js
+++ js/eye-blink.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
   // Timings
   
   // For setInterval, the animation interval will be double this.
-  var eye1 = 2000;
-  var eye2 = 4700;
-  var eye3 = 1400;
-  var eye4 = 2800;
-  var eye5 = 3000;
-  var eye6 = 2000;
+  var eye1 = 9000;
+  var eye2 = 15700;
+  var eye3 = 17000;
+  var eye4 = 16800;
+  var eye5 = 12000;
+  var eye6 = 6000;
   
   // Option #1 - Fade in at different intervals.
-  $("#eye1").hide().delay(eye1).fadeIn("slow").addClass("eye-blink");
-  $("#eye2").hide().delay(eye2).fadeIn("slow").addClass("eye-blink");
-  $("#eye3").hide().delay(eye3).fadeIn("slow").addClass("eye-blink");
-  $("#eye4").hide().delay(eye4).fadeIn("slow").addClass("eye-blink");
-  $("#eye5").hide().delay(eye5).fadeIn("slow").addClass("eye-blink");
-  $("#eye6").hide().delay(eye6).fadeIn("slow").addClass("eye-blink");
+  $("#eye1").hide().delay(3200).fadeIn("slow");
+  $("#eye2").hide().delay(3600).fadeIn("slow");
+  $("#eye3").hide().delay(4800).fadeIn("slow");
+  $("#eye4").hide().delay(3800).fadeIn("slow");
+  $("#eye5").hide().delay(5000).fadeIn("slow");
+  $("#eye6").hide().delay(3000).fadeIn("slow");
   
   // Option #2 - Fade in all at once.
   /*
