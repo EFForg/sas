ef740e008639c74d0bad6e47d3047ba8d7a12dac
mark burdett <mfb@eff.org>
2014-06-25 15:03:01 -0700

    add scorecard tablesort function

--- docs/js/scorecard.js
+++ docs/js/scorecard.js
@@ -1,7 +1,12 @@
+var scorecardTextExtraction = function(node) {
+  return node.childNodes[0] ? node.childNodes[0].innerHTML : node.innerHTML;
+}
+
 $(document).ready(function() {
   // Add tablesorter and properties.
   $('#scorecard-body').html(sasDisplayAllReps());
   $('#scorecard-table').tablesorter({
+    textExtraction: scorecardTextExtraction,
     sortList: [[1,0]],
     headers: {
       4: {
