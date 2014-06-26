8b96fb2984a3291ab2afbbd4b8a020bfa18d7d46
mark burdett <mfb@eff.org>
2014-06-26 15:08:40 -0700

    add csv-to-json script

---
+++ scripts/csv-to-json.php
@@ -0,0 +1,11 @@
+<?php
+$lines = file('https://raw.githubusercontent.com/sunlightlabs/eff_scorecard/master/scores.csv');
+$reps= new stdClass();
+foreach($lines as $line) {
+  $data = str_getcsv($line);
+  $key = $data[0];
+  array_splice($data, 10, 1);
+  array_shift($data);
+  $reps->{$key} = $data;
+}
+file_put_contents('json', json_encode($reps));
