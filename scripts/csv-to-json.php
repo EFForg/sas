08caf1332b93b165c5e7b4dfe094c5d49dac02c5
thomasdavis <thomasalwyndavis@gmail.com>
2014-08-04 22:20:23 +1000

    updated scores

--- scripts/csv-to-json.php
+++ scripts/csv-to-json.php
@@ -4,8 +4,9 @@ $reps= new stdClass();
 foreach($lines as $line) {
   $data = str_getcsv($line);
   $key = $data[0];
-  array_splice($data, 10, 1);
+  array_splice($data, 11, 1);
   array_shift($data);
   $reps->{$key} = $data;
 }
 file_put_contents('json', json_encode($reps));
+	
